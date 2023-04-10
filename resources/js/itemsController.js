a335e36eb81701add9348cbd339596fc74c7bb98
pravalika2571 <vangapravalika.123@gmail.com>
2023-04-10 12:46:42 +0530

    Add files

---
+++ resources/js/itemsController.js
@@ -0,0 +1,30 @@
+class ItemsController {
+    // Set up the items and currentId property in the contructor
+    constructor(currentId = 0) {
+        this.items = [];
+        this.currentId = currentId;
+    }
+
+    // Create the addItem method
+    addItem(name, description,imageUrl) {
+        const item = {
+            // Increment the currentId property
+            id: this.currentId++,
+            name: name,
+            description: description,
+            imageUrl: imageUrl
+        };
+
+        // Push the item to the items property
+        this.items.push(item);
+    }
+}
+// class ItemsController{
+//     constructor(){
+//         this.items = [];
+
+//     }
+   
+// }
+// ItemsController = new ItemsController();
+// console.log(ItemsController.items);
